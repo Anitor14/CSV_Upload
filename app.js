b989f60384beab60929bbb959ca6b90fbf14de38
Anitor <anitorabraham@gmail.com>
2022-02-02 10:14:08 +0100

    update essential dependencies

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
 const express = require("express");
 const app = express();
-// const upload
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const cron = require("node-cron");
+const cors = require("cors");
+
+app.enable("trust proxy");
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.options("*", cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 20000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({ whitelist: ["duration", "ratingsQuantity", "ratingsAverage", "maxGroupSize", "difficulty", "price"] })
+);
+
+app.use(compression());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
 
 // product router
 const uploadsRouter = require("./routes/uploadRoutes");
@@ -9,7 +66,7 @@ const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //middleware
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
